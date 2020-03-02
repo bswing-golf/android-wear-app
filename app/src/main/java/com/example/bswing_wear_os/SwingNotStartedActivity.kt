43f5e5e929bada120990c687ad14dcc0a8136570
stipanciktomislav <stipanciktomislav@gmail.com>
2020-03-01 17:05:51 -0800

    Added state for when `Do Not Disturb` mode is true

--- app/src/main/java/com/example/bswing_wear_os/SwingNotStartedActivity.kt
+++ app/src/main/java/com/example/bswing_wear_os/SwingNotStartedActivity.kt
@@ -1,8 +1,11 @@
 package com.example.bswing_wear_os
 
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.view.View
 import kotlinx.android.synthetic.main.swing_not_started.*
 
 class SwingNotStartedActivity : WearableActivity() {
@@ -12,6 +15,12 @@ class SwingNotStartedActivity : WearableActivity() {
         setContentView(R.layout.swing_not_started)
 
         Helper.setCurrentContextInstance(this)
+
+        this.checkInterruptionFilter()
+
+        REFRESH_BUTTON.setOnClickListener {
+            this.checkInterruptionFilter()
+        }
 
         START_SWING_BUTTON.setOnClickListener {
             this.startSwing()
@@ -27,6 +36,11 @@ class SwingNotStartedActivity : WearableActivity() {
         setAmbientEnabled()
     }
 
+    override fun onResume() {
+        super.onResume()
+        this.checkInterruptionFilter()
+    }
+
     private fun startSwing() {
         val intent = Intent(this, StartingSwingActivity::class.java)
         startActivity(intent)
@@ -35,6 +49,19 @@ class SwingNotStartedActivity : WearableActivity() {
     private fun goToSelectClubView() {
         val intent = Intent(this, SelectClubActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun checkInterruptionFilter() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (notificationManager.currentInterruptionFilter == 2) {
+            DO_NOT_DISTURB_TEXT.visibility = View.VISIBLE
+            REFRESH_BUTTON.visibility = View.VISIBLE
+            START_SWING_BUTTON.visibility = View.GONE
+        } else {
+            DO_NOT_DISTURB_TEXT.visibility = View.GONE
+            REFRESH_BUTTON.visibility = View.GONE
+            START_SWING_BUTTON.visibility = View.VISIBLE
+        }
     }
 
 }
